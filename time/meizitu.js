ad93ab230bd353c5e950326bb080fb7fd3ca47b2
Yuehuaer <123544071+Yuehuaer@users.noreply.github.com>
2025-03-28 18:21:44 +0800

    Update meizitu.js

--- time/meizitu.js
+++ time/meizitu.js
@@ -27,7 +27,7 @@ const MODES = {
 }
 
 !(async () => {
-  let mode = ($.lodash_get(arg, 'MODE') || '0')
+  let mode = ($.lodash_get(arg, 'MODE') || '1,2')
     .split(/,|，/gi)
     .map(i => i.trim())
     .filter(i => i.length)
