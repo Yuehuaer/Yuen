119a02de93de02622721e2d4c55a3d0a497cb5b5
慕时沙 <123544071+Yuehuaer@users.noreply.github.com>
2025-07-02 15:15:18 +0800

    Update Zyue.js

--- time/Zyue.js
+++ time/Zyue.js
@@ -1,6 +1,8 @@
 /*
 脚本名称：万夜图片
-脚本作者：YueJS
+脚本作者：YueJSMore actions
+更新时间：2024-03-21
+脚本说明：点击通知查看图片
 测试版本：Loon iOS 17
 */
 
@@ -8,29 +10,28 @@ const NAME = 'wanye'
 const $ = new Env(NAME)
 
 // API列表，可以通过修改 API_INDEX 的值来切换不同API
-const API_INDEX = 7  // 当前使用的API序号：1、2、3、4、5、6、7
+const API_INDEX = 2  // 当前使用的API序号：1、2、3或4
+const API_INDEX = 4  // 当前使用的API序号：1、2、3或4
 
 const API_LIST = {
-    1: 'http://3650000.xyz/api/?type=img&mode=6',     // API1,随机美女图，部分
-    2: 'https://api.lolimi.cn/API/meinv/api.php?type=image',  // API2,随机萝莉图，画质极好
-    3: 'https://www.onexiaolaji.cn/RandomPicture/api/?key=qq249663924',  // API3，随机美女，不能刷新
-    4: 'https://v2.xxapi.cn/api/meinvpic?return=302',  // API4，随机美女，不能刷新
-    5: 'http://3650000.xyz/api/360.php?cid=6',   // API5，360美女
-    6: 'http://3650000.xyz/api/?type=img',   // API6,公共图床速度快~
-    7: 'http://3650000.xyz/api/?type=img&mode=1,3,5,6,7,8,9'   // API6,可多选随机返回
+    1: 'http://3650000.xyz/api/360.php?cid=6',     // API1
+    2: 'https://api.lolimi.cn/API/meinv/api.php?type=image',  // API2
+    3: 'https://www.onexiaolaji.cn/RandomPicture/api/?key=qq249663924',  // API3
+    4: 'https://v2.xxapi.cn/api/meinvpic?return=302'  // API4
+    // 5: '在这里添加第五个API'   // API5
 }
 
 !(async () => {
     const url = API_LIST[API_INDEX]
     if (!url) throw new Error('API序号无效')
-    
+
     // 獲取重定向後的真實圖片地址
     const realImageUrl = await http({
         url: url
     })
-    
+
     if (!realImageUrl) throw new Error('获取图片地址失败')
-    
+
     await notify(NAME, `API-${API_INDEX} 获取成功`, `${realImageUrl}`, {
         'open-url': realImageUrl,
         'media-url': realImageUrl
