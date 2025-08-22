e7bd9cffa983253dec64c2227cd937631378536d
慕时沙 <123544071+Yuehuaer@users.noreply.github.com>
2025-08-22 11:21:13 +0800

    Update block_ads.js

--- Script/block_ads.js
+++ Script/block_ads.js
@@ -1,6 +1,12 @@
 // Loon 脚本：屏蔽 pushplus 的广告图片
-if ($request.url.indexOf("/ad/") != -1) {
+if ($request.url.indexOf("image.pushplus.plus/ad/") != -1) {
   $done({ response: { status: 200, headers: { "Content-Type": "image/png" }, body: "" } });
-} else {
-  $done({});
 }
+
+// Loon 脚本：屏蔽 file.youpiaopiaopiao.cn 的广告图片
+if ($request.url.indexOf("file.youpiaopiaopiao.cn/upload/materials/") != -1) {
+  $done({ response: { status: 200, headers: { "Content-Type": "image/jpeg" }, body: "" } });
+}
+
+// 默认情况，不做处理
+$done({});
